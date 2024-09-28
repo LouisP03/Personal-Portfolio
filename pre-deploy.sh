42ed9097eb4e7ad2ed0a5c27656176f99ae255cc
Louis Parkes <Louis.Parkes@coxautoinc.com>
2024-09-28 14:54:18 +0100

    pre-deploy.sh now creates a CNAME record

--- pre-deploy.sh
+++ pre-deploy.sh
@@ -7,4 +7,10 @@ rmdir docs/browser || rm -r docs/browser
 
 cp docs/index.html docs/404.html
 
+echo "www.louisparkes.co.uk" > docs/CNAME
+
+#sed -i 's/^[ \t]*//;s/[ \t]*$//' docs/3rdpartylicenses.txt
+rm docs/3rdpartylicenses.txt || true
+
+
 git add .
